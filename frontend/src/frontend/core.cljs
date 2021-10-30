2317e2e000c27b72f372167b9437f39d810a474c
Hoseong Son <me@sookcha.com>
2021-10-31 01:16:27 +0900

    Add aoc to frontend/deps.edn

--- frontend/src/frontend/core.cljs
+++ frontend/src/frontend/core.cljs
@@ -1,13 +1,14 @@
 (ns frontend.core
     (:require
       [reagent.core :as r]
-      [reagent.dom :as d]))
+      [reagent.dom :as d]
+      [aoc.year2018.day07 :as day07]))
 
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+        [:div [:h2 "Welcome to Reagent"]])
 
 ;; -------------------------
 ;; Initialize app
--- project.clj
+++ project.clj
@@ -1,4 +1,2 @@
-(defproject sookcha/advent-of-code "1.0.0-SNAPSHOT" ; version "1.0.0-SNAPSHOT"
-  :eval-in :leiningen
-  :source-paths ["src"]
-  :dependencies [[org.clojure/core.match "1.0.0"]])
+(defproject sookcha/advent-of-code "1.0.0-SNAPSHOT"         ; version "1.0.0-SNAPSHOT"
+  :eval-in :leiningen)
